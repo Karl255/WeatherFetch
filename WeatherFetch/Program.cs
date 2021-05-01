2e92acf07e49845556d15d75a08596a86a00aa55
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-05-01 02:25:08 +0200

    Add project files.

---
+++ WeatherFetch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace WeatherFetch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HttpWebRequest request = WebRequest.CreateHttp("http://api.weatherapi.com/v1/current.json?");
+		}
+	}
+}
