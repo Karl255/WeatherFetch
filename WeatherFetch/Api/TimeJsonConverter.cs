d748ff4536266d9ef9b6b71aeb548bb1a855c859
Karlo <49595438+Karl255@users.noreply.github.com>
2021-09-12 08:42:11 +0200

    Fixed crash when the API gives the moonrise/moonset field with an invalid value (such as "No moonset").

--- WeatherFetch/Api/TimeJsonConverter.cs
+++ WeatherFetch/Api/TimeJsonConverter.cs
@@ -4,19 +4,30 @@ using System.Text.Json.Serialization;
 
 namespace WeatherFetch.Api
 {
-	internal class TimeJsonConverter : JsonConverter<DateTime>
+	internal class TimeJsonConverter : JsonConverter<DateTime?>
 	{
 		private const string DateTimeFormat = "hh:mm tt";
 
-		public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.TokenType switch
+		public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 		{
-			// if unix epoch time
-			JsonTokenType.Number => DateTimeOffset.FromUnixTimeSeconds(reader.GetInt64()).DateTime,
-			JsonTokenType.String => DateTime.ParseExact(reader.GetString(), DateTimeFormat, null),
-			_ => throw new Exception("Invalid JSON: trying to parse DateTime from unsupported format.")
-		};
+			try
+			{
+				return reader.TokenType switch
+				{
+					// if unix epoch time
+					JsonTokenType.Number => DateTimeOffset.FromUnixTimeSeconds(reader.GetInt64()).DateTime,
+					JsonTokenType.String => DateTime.ParseExact(reader.GetString(), DateTimeFormat, null),
+					_ => throw new Exception("Invalid JSON: trying to parse DateTime from unsupported format.")
+				};
+			}
+			catch
+			{
+				// because sometimes the API likes to put invalid time values into some time fields
+				return null;
+			}
+		}
 
-		public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) =>
-			writer.WriteStringValue(value.ToString(DateTimeFormat));
+		public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options) =>
+			writer.WriteStringValue(value is null ? "" : value.Value.ToString(DateTimeFormat));
 	}
 }
